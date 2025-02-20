a150d1d4cc8cbfd4ff3ec49091cb163320f234d7
DESKTOP-UCETDSH\andre <andrewmcl6081@gmail.com>
2025-02-20 16:59:21 -0600

    Modified styling

--- forager/components/NavBar.jsx
+++ forager/components/NavBar.jsx
@@ -1,26 +1,36 @@
+"use client"
+
 import Link from 'next/link';
+import { usePathname } from 'next/navigation';
 import { FaHome, FaSearch } from 'react-icons/fa';
 import { TbMushroom } from "react-icons/tb";
-import styles from '../styles/NavBar.module.css'; // Import the styles for the NavBar
 
 export default function NavBar() {
+  const pathname = usePathname();
+  
   return (
-    <div className={styles.navbar}>
-      <Link href="/mushroom" passHref>
-        <div className={styles.navItem}>
-          <TbMushroom />
-        </div>
-      </Link>
-      <Link href="/dashboard" passHref>
-        <div className={styles.navItem}>
-          <FaHome />
-        </div>
-      </Link>
-      <Link href="/photosearch" passHref>
-        <div className={styles.navItem}>
-          <FaSearch />
-        </div>
-      </Link>
+    <div className="fixed bottom-0 left-0 right-0 bg-[#579076] shadow-lg p-3 flex flex-col items-center z-50 rounded-t-2xl">
+      <div className="flex justify-around w-full">
+        <Link href="/mushroom" passHref>
+          <div className="text-2xl cursor-pointer flex justify-center items-center p-3">
+            <TbMushroom color={pathname === "/mushroom" ? "#B6EEA6" : "#FFFFFF"}/>
+          </div>
+        </Link>
+        <Link href="/dashboard" passHref>
+          <div className="text-2xl cursor-pointer flex justify-center items-center p-3">
+            <FaHome color={pathname === "/dashboard" ? "#B6EEA6" : "#FFFFFF"}/>
+          </div>
+        </Link>
+        <Link href="/photosearch" passHref>
+          <div className="text-2xl cursor-pointer flex justify-center items-center p-3">
+            <FaSearch color={pathname === "/photosearch" ? "#B6EEA6" : "#FFFFFF"}/>
+          </div>
+        </Link>
+      </div>
+
+      <div className="flex justify-center w-full">
+        <div className="w-24 h-1 bg-white rounded-full"/>
+      </div>
     </div>
   );
 }
