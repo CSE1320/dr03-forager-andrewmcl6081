31b6df306fc4302e0926f52c0b38190aefe1262c
DESKTOP-UCETDSH\andre <andrewmcl6081@gmail.com>
2025-02-21 11:36:12 -0600

    Began implementing dashboard

--- forager/app/dashboard/page.jsx
+++ forager/app/dashboard/page.jsx
@@ -1,8 +1,18 @@
+import Image from "next/image";
 
 export default function DashboardPage() {
   return (
-    <div className="page">
-      <h1>Dashboard Page</h1>
+    <div className="w-full h-screen bg-[#579076] relative overflow-hidden">
+      {/* Mushroom Vector */}
+      <div className="absolute top-0 right-0 z-0">
+        <Image 
+          src="/images/mushroom_vector.svg"
+          alt="Mushroom Vector"
+          height={200}
+          width={200}
+          priority
+        />
+      </div>
     </div>
   );
 }
